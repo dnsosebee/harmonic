529d7b0ef7a67244a51baecf9fa513f14639d161
Daniel Sosebee <dnsosebee@gmail.com>
2023-12-28 17:05:10 -0800

    styles updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 space-y-8">
-      <Image src="/harmonic-icon.svg" width={200} height={200} alt="Harmonic" />
-      <p>Coming soon</p>
-    </main>
+    <>
+      <main
+        className="flex min-h-screen flex-col items-center p-24 space-y-8"
+        style={{
+          backgroundColor: "#181918",
+          backgroundImage: `url("/logo-dark.svg")`,
+          backgroundRepeat: "repeat",
+          backgroundSize: "10%",
+        }}
+      >
+        <Image
+          src="/harmonic-icon.svg"
+          width={200}
+          height={200}
+          alt="Harmonic"
+          className="shadow-2xl"
+        />
+        <p className="text-gray-500 shadow-2xl">Coming soon</p>
+      </main>
+    </>
   );
 }
