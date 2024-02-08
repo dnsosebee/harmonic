093310b9f1fbb17791657c55abdfa305d9f959a9
Daniel Sosebee <dnsosebee@gmail.com>
2024-02-07 17:09:29 -0800

    light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen flex flex-col items-stretch bg-slate-800`}>
+      <body className={`${inter.className} min-h-screen flex flex-col items-stretch bg-white`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,41 +4,43 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header className="bg-white">
+      <header className="bg-white border-b-1 border border-gray-200 shadow py-2">
         <Link href={"/"}>
           <Image
             src="/harmonic-wordmark.svg"
             alt="Harmonic Software logo"
-            width={200}
+            width={150}
             height={40}
-            className="bg-green-200 px-2  m-3 shadow-lg outline outline-1 outline-green-300 rounded-sm"
+            className=""
           />
         </Link>
       </header>
       <main
-        className="flex  flex-col items-center sm:p-10 lg:p-16 grow"
+        className="flex  flex-col items-stretch grow self-stretch"
         style={{
-          backgroundImage: `url("/harmonic-tesselation-dark.svg")`,
+          backgroundImage: `url("/harmonic-tesselator.svg")`,
           backgroundRepeat: "repeat",
           backgroundSize: "6.25%",
         }}
       >
-        <section className="space-y-6 prose flex flex-col bg-slate-900 py-6 sm:px-6 shadow-2xl rounded-xl">
-          <Image
-            src="/harmonic-icon.svg"
-            width={200}
-            height={200}
-            alt="Harmonic"
-            className="shadow-2xl self-center rounded"
-          />
-          <p className="text-slate-200 shadow-2xl">
-            Harmonic Software is a consultancy specializing in AI applications. We work closely with
-            our clients throughout the entire product lifecycle, aiding in design, production, and
-            maintenance of AI systems. Contact: info@harmonic.so
-          </p>
-        </section>
+        <div className="flex flex-col items-center grow bg-white bg-opacity-95">
+          <section className="space-y-6 prose flex flex-col bg-white py-6 sm:px-6 grow border-x-2 border-gray-200">
+            <Image
+              src="/harmonic-logo.svg"
+              width={100}
+              height={100}
+              alt="Harmonic"
+              className="self-center"
+            />
+            <p className="">
+              Harmonic is a consultancy specializing in AI applications. We work closely with our
+              clients throughout the entire AI product lifecycle — design, production, and beyond.
+              Contact: info@harmonic.so
+            </p>
+          </section>
+        </div>
       </main>
-      <footer className="">
+      <footer className="border border-t-2 border-gray-200 shadow p-2">
         <p>Contact: info@harmonic.so</p>
         <p>© 2024 Harmonic Software LLC. All rights reserved.</p>
       </footer>
