059877170a46d0848f45f23c03c508d8b1031b24
Daniel Sosebee <dnsosebee@gmail.com>
2024-02-25 15:24:13 -0800

    astro

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      favicon: "/favicon.ico",
+      title: "Harmonic Software",
+      description: "Harmonic software is a trusted software agency for the age of AI.",
+      // logo: {
+      //   light: "/logo/harmonic-logo-light.svg",
+      //   dark: "/logo/harmonic-logo-dark.svg",
+      // },
+      social: {
+        linkedin: "https://www.linkedin.com/company/harmonic-software-llc/",
+      },
+      sidebar: [
+        {
+          label: "Info",
+          items: [{ label: "About", link: "/info/about/" }],
+        },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+});
---
+++ src/content/docs/info/about.md
@@ -0,0 +1,10 @@
+---
+title: About Us
+description: About page for Harmonic Software.
+---
+
+Harmonic Software is a consultancy specializing in AI applications. We work closely with our clients throughout the entire AI product lifecycle — design, production, and beyond. Contact: info@harmonic.so
+
+:::note
+The rest of this website will come soon. For now, we're too busy shipping 🚀
+:::
