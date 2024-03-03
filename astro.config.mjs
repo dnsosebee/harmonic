9d74e3271549e7d32e92f4a706205bc6a6ed449e
Daniel Sosebee <dnsosebee@gmail.com>
2024-03-02 16:00:13 -0800

    simple astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,11 @@
-import starlight from "@astrojs/starlight";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      favicon: "/favicon.ico",
-      title: "Harmonic Software",
-      description: "Harmonic software is a trusted software agency for the age of AI.",
-      logo: {
-        light: "./src/assets/logo/harmonic-logo-light.svg",
-        dark: "./src/assets/logo/harmonic-logo-dark.svg",
-      },
-      social: {
-        linkedin: "https://www.linkedin.com/company/harmonic-software-llc/",
-      },
-      sidebar: [
-        {
-          label: "Info",
-          items: [{ label: "About", link: "/info/about/" }],
-        },
-        // {
-        //   label: "Reference",
-        //   autogenerate: { directory: "reference" },
-        // },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
